3870debd55a42004cf01f05f20a62ecceda29414
TheHowdyTooter <wabrady51@gmail.com>
2025-11-08 15:58:39 -0500

    Create/Study prev/next updates

--- src/routes/Create.tsx
+++ src/routes/Create.tsx
@@ -11,6 +11,7 @@ function Create()
 {
     const createRef = useRef(new CreateMode());
     const createInstance = createRef.current;
+    const [dummyState, setdummyState] = useState(1)
     const [question, setQuestion] = useState("");
     const [answer, setAnswer] = useState("");
 
@@ -21,13 +22,25 @@ function Create()
         localStorage.setItem('deck', JSON.stringify(createInstance.getDeck()));
     }
 
+    const prevCard = () => (
+        createInstance.goToPrevious(),
+        setdummyState(dummyState * -1)
+    );
+
+    const nextCard = () => (
+        createInstance.goToNextCard(),
+        setdummyState(dummyState * -1)
+    );
+
     return (
         <div>
             <div>Create</div>
             <form className="answerSubmission" onSubmit={handleSubmit}>
                 <input type="text" onChange={(e) => setQuestion(e.target.value)}/><br/>
                 <input type="text" onChange={(e) => setAnswer(e.target.value)}/><br/>
+                <button type={prevCard}>Prev Card</button>
                 <button type='submit'>Add Card</button>
+                <button type={nextCard}>Next Card</button>
             </form>
         </div>
     );
