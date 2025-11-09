c82057b6a6e1f973cc1524b1e85913b7101ccde8
jcumberamon10 <jcumbe1011@gmail.com>
2025-11-08 23:13:09 -0500

    CardAppendArr function

--- src/storage/storestring.tsx
+++ src/storage/storestring.tsx
@@ -1,22 +1,10 @@
 //import { useState } from "react";
 import { db } from "../firebase";
 //import { auth, firestore } from "../firebase";
-import { addDoc,collection,doc,getDoc } from "firebase/firestore";
+import { addDoc,collection,doc,setDoc,getDoc,updateDoc,arrayUnion } from "firebase/firestore";
 //import {onSnapshot, setDoc, updateDoc, arrayUnion, serverTimestamp} from "firebase/firestore";
 //import { getFirestore } from "firebase/firestore";
 
-/*
-export default async function AddToFlashcardArr(){
-    console.log("This function is being called")
-    const docData = {
-        ans: "TEST_ANSWER5",
-        fc_string: "TEST_THING5"
-    };
-    setDoc(flashcards,docData,{merge: true}).then(() => {
-        console.log('VALUE HAS BEEN WRITTEN');
-    }
-)};
-*/
 
 
 export  async function GetCurrentDeckID(){
@@ -28,9 +16,24 @@ export async function GetCardArray(){
     const deckID = await GetCurrentDeckID();
     const deckRef = await getDoc(doc(db,"decks",String(deckID),));
     return deckRef.data()?.cards ?? [];
+}
+
+export async function AppendCardArray(){
+    const deckID = await GetCurrentDeckID();
+    //const deckRef = await getDoc(doc(db,"decks",String(deckID),));
+   // console.log(deckRef.data()?.cards ?? [])
+    const deckRef = await doc(db,"decks",String(deckID));
+    
+    const newItem1 = "cardStringTest";
+    await setDoc(
+        deckRef,
+        {cards: arrayUnion(newItem1)},
+        {merge: true}
+    ) ;
 
 }
-//const flashcards = doc(db, 'flashcard/SEBKcU79uLCVOnJZKzzZ');
+
+
 export async function addCardCurrentDeck(){
 
 }
@@ -38,61 +41,18 @@ export default async function AddToFlashcardArr() {
 
   console.log("This function is being called");
   GetCardArray();
-
-
-  const docData = {
-    ans: "TEST_ANSWER6",
-    fc_string: "TEST_THING6",
-  };
-
-  try {
-    const docRef = await addDoc(collection(db, "flashcard"), docData);
-    console.log(" Added new flashcard with ID:", docRef.id);
-  } catch (e) {
-    console.error(" Error adding flashcard:", e);
-  }
+  AppendCardArray();
 }
 
 
 
-// export async function getOwnedDecksField()
-// {
-//   const test = await getDoc(doc(db, "users", "sample_user_id"));
-//   console.log("DATA: " + test.data().decks);
-// }
 
-// export async function readASingleDocument(){
-//     const mySnapShot = await getDoc(flashcards);
-//     if(mySnapShot.exists()){
-//         const docData = mySnapShot.data();
-//         console.log('Trying to read Document');
-//         console.log(`Snapshot Data is ${JSON.stringify(docData)}`)
-//     }   
-// };
 
-// export function listenToADocument() {
-//   onSnapshot(flashcards, (docSnapshot) => {
-//     if (docSnapshot.exists()) {
-//       const docData = docSnapshot.data();
-//       console.log('Trying to listen to document');
-//       console.log(`In realtime. docdata is ${JSON.stringify(docData)}`);
-//     }
-//   });
-// }
 
-      /*
-    //const flashcard = doc(firestore, 'fcArray/tl6kFGnUf1jd9xEbE6Z7');
-    const docRef = doc(db,"fcArray","tl6kFGnUf1jd9xEbE6Z7");
 
-   await setDoc(
-    docRef,
-    {
-      fcQuesArr: arrayUnion("Test Questions Firebase"),
-      fcAnsArr: arrayUnion("FirebaseAns"),
-    },
-    { merge: true } 
-  );
-  */
+
+
+    
 
 
 
