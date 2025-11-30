39a231f9e1fe635d587d5b298c27118a9b4e66d8
orfnyc <ollie.r.fordham@gmail.com>
2025-11-30 12:52:59 -0500

    Setup for better question and answer storage

---
+++ src/midlayer/FunctionToken.ts
@@ -0,0 +1,6 @@
+import Token from './Token'
+
+export default class FunctionToken extends Token
+{
+    
+}
---
+++ src/midlayer/VariableToken.ts
@@ -0,0 +1,5 @@
+import Token from './Token'
+export default class VariableToken extends Token
+{
+
+}
